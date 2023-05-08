c2b9c24aa9ea9b3b656d582521930672be297237
halil <halilakgun0@gmail.com>
2023-05-08 20:44:33 +0300

    Initial commit

---
+++ src/Q01_FindDifferentElements.java
@@ -0,0 +1,16 @@
+/*
+Aşağıdaki soruları verilen matrisler kullanarak cevaplayınız. Algoritmanın akış diyagramını çiziniz.
+
+        3 6 2          1 1 2
+   A =  1 5 6     B =  3 3 3
+        2 1 5          2 2 1
+
+    01. A dizisinde olup B dizisinde olmayan elemanları bulunuz
+ */
+public class Q01_FindDifferentElements {
+    public static void main(String[] args) {
+        int[][] arrA = {{3, 6, 2}, {1, 5, 6}, {2, 1, 5}};
+        int[][] arrB = {{1, 1, 2}, {3, 3, 3}, {2, 2, 1}};
+
+    }
+}
